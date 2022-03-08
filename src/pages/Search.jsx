2ce89f9100328b46d2d795155bca0a35a2507a11
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2022-03-08 17:59:40 -0300

    Requisito 6 OK

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,22 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
       buttonDisabled: true,
+      loading: false,
+      inputValue: '',
+      artist: '',
+      albums: '',
+      albumsEmpty: true,
+      returnText: '',
+      cards: '',
     };
     this.onInputChange = this.onInputChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.displayResponse = this.displayResponse.bind(this);
   }
 
-  handleClick() {
+  async handleClick() {
+    this.setState({ loading: true });
+    const { inputValue } = this.state;
+    const response = await searchAlbumsAPI(inputValue);
+    this.setState({ inputValue: '' });
+    this.setState({ buttonDisabled: true });
+    this.setState({ albums: response });
+    this.setState({ loading: false });
+    this.displayResponse();
   }
 
   onInputChange({ target }) {
     const MIN_LENGTH = 2;
     const { value } = target;
+    this.setState({
+      inputValue: value,
+      artist: value,
+    });
     if (value.length >= MIN_LENGTH) {
       this.setState({ buttonDisabled: false });
     } else {
@@ -24,31 +47,82 @@ class Search extends React.Component {
     }
   }
 
+  displayResponse() {
+    const { albums, artist } = this.state;
+    if (albums.length > 0) {
+      this.setState({ albumsEmpty: false });
+      this.setState({ returnText: `Resultado de álbuns de: ${artist}` });
+      this.setState({
+        cards: (
+          <div className="card-display">
+            {albums.map((album) => (
+              <Link
+                key={ album.collectionId }
+                to={ `/album/${album.collectionId}` }
+                data-testid={ `link-to-album-${album.collectionId}` }
+              >
+                <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+                <p>{album.collectionName}</p>
+              </Link>
+            ))}
+          </div>
+        ),
+      });
+    } else {
+      this.setState({ albumsEmpty: true });
+      this.setState({ returnText: 'Nenhum álbum foi encontrado' });
+    }
+  }
+
   render() {
-    const { buttonDisabled } = this.state;
+    const {
+      buttonDisabled,
+      loading,
+      inputValue,
+      albumsEmpty,
+      returnText,
+      cards,
+    } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <p>Search</p>
-        <form>
-          <label htmlFor="artist">
-            <input
-              type="text"
-              name="artist"
-              data-testid="search-artist-input"
-              placeholder="Nome do Artista"
-              onChange={ this.onInputChange }
-            />
-          </label>
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            onClick={ this.handleClick }
-            disabled={ buttonDisabled }
-          >
-            Pesquisar
-          </button>
-        </form>
+        {loading
+          ? <Loading />
+          : (
+            <div>
+              <form>
+                <label htmlFor="artist">
+                  <input
+                    type="text"
+                    name="artist"
+                    data-testid="search-artist-input"
+                    placeholder="Nome do Artista"
+                    value={ inputValue }
+                    onChange={ this.onInputChange }
+                  />
+                </label>
+                <button
+                  type="button"
+                  data-testid="search-artist-button"
+                  onClick={ this.handleClick }
+                  disabled={ buttonDisabled }
+                >
+                  Pesquisar
+                </button>
+              </form>
+            </div>
+          )}
+        <div>
+          {albumsEmpty
+            ? <p>{returnText}</p>
+            : (
+              <>
+                <p>{returnText}</p>
+                {cards}
+              </>
+            )}
+        </div>
       </div>
     );
   }
